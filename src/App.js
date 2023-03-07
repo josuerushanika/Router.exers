d37c02c73e41ab8d1c845c72e2fc1a0e1f0e5c8b
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-07 16:55:18 +0200

    Adding tree of route in  createBrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
  // eslint-disable-next-line
-import { BrowserRouter,Routes,Route,Link,NavLink } from "react-router-dom";
+import { 
+  createBrowserRouter,
+  Routes,
+  Route,
+  Link,
+  NavLink, 
+  createRoutesFromElements
+} from "react-router-dom";
 
 //Pages
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Routes>
+        
+    <Route index element={<Home/>}/>
+    <Route path="/about" element={<About/>}/>
+  </Routes>
+
+  )
+);
 function App() {
   return (
    
-    <header>
-      <nav>
-        <h1>JobaRouter</h1>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="About">About</NavLink>
-      </nav>
-    </header>
+    // <header>
+    //   <nav>
+    //     <h1>JobaRouter</h1>
+    //     <NavLink to="/">Home</NavLink>
+    //     <NavLink to="About">About</NavLink>
+    //   </nav>
+    // </header>
      <main>
       <Routes>
         
